b9a46c8ef1de23e975fdcd3378b5398a8a555d60
er2427 <elise.raschke@marquette.edu>
2024-11-05 13:52:30 -0600

    new data types

--- app/src/main/java/com/example/where2watch/ui/model/Movie.java
+++ app/src/main/java/com/example/where2watch/ui/model/Movie.java
@@ -3,18 +3,25 @@ package com.example.where2watch.ui.model;
 public class Movie {
     private String title;
     private String genre;
+    private String[] platform;
     private float rating;
     private int imageResId;
 
-    public Movie(String title, String genre, float rating, int imageResId) {
+    public Movie(String title, String genre, String[] platform, float rating, int imageResId) {
         this.title = title;
         this.genre = genre;
+        this.platform = platform;
         this.rating = rating;
         this.imageResId = imageResId;
     }
 
     public String getTitle() { return title; }
     public String getGenre() { return genre; }
+
+    public String[] getPlatform() {
+        return platform;
+    }
+
     public float getRating() { return rating; }
     public int getImageResId() { return imageResId; }
 }
