708fde3790688711ad2f0cfee73ec37cc79a7d5c
er2427 <elise.raschke@marquette.edu>
2024-11-05 13:39:45 -0600

    initial design

--- app/src/main/java/com/example/where2watch/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/where2watch/ui/search/SearchFragment.java
@@ -4,29 +4,122 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.appcompat.widget.SearchView;
 
 import com.example.where2watch.databinding.FragmentHomeBinding;
+import com.example.where2watch.R;
+import com.example.where2watch.ui.adapter.MovieAdapter;
+import com.example.where2watch.ui.model.Movie;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class SearchFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private SearchView searchView;
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+    ChipGroup chipGroup;
+    Set<String> selectedItems = new HashSet<>();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        SearchViewModel searchViewModel =
-                new ViewModelProvider(this).get(SearchViewModel.class);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        String[] item = {"Disney+", "Hulu", "Peacock", "Netflix"};
 
-        final TextView textView = binding.textHome;
-        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        autoCompleteTextView = view.findViewById(R.id.auto_complete_txt);
+        adapterItems = new ArrayAdapter<>(requireContext(), R.layout.filter_list_item, item);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = parent.getItemAtPosition(position).toString();
+
+                // Check if the item is already selected
+                if (!selectedItems.contains(selectedItem)) {
+                    // Add chip to ChipGroup
+                    addChip(selectedItem);
+                    // Add the selected item to the set
+                    selectedItems.add(selectedItem);
+                } else {
+                    // Notify the user or handle the case where the item is already selected
+                    Toast.makeText(requireContext(), "Item already selected", Toast.LENGTH_SHORT).show();
+                }
+
+                // Clear the text in AutoCompleteTextView after selection
+                autoCompleteTextView.setText("");
+            }
+        });
+
+        chipGroup = view.findViewById(R.id.chipGroup);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        searchView = view.findViewById(R.id.searchView);
+
+        // Set up the search view to filter the recycler view
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // Pass information to the search results
+                Bundle bundle = new Bundle();
+                bundle.putString("search_query", query);
+                bundle.putStringArrayList("filter_items", new ArrayList<>(selectedItems));
+
+                NavController navController = Navigation.findNavController(view);
+                navController.navigate(R.id.action_searchFragment_to_searchResultsFragment, bundle);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        searchView.clearFocus();
+
+    }
+
+    private void addChip(String text) {
+        Chip chip = new Chip(requireContext());
+        chip.setText(text);
+        chip.setCloseIconVisible(true);
+        chip.setOnCloseIconClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chipGroup.removeView(chip);
+                selectedItems.remove(text);
+            }
+        });
+
+        chipGroup.addView(chip);
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/where2watch/ui/search/SearchResults.java
@@ -0,0 +1,103 @@
+package com.example.where2watch.ui.search;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.where2watch.R;
+import com.example.where2watch.databinding.FragmentHomeBinding;
+import com.example.where2watch.ui.adapter.MovieAdapter;
+import com.example.where2watch.ui.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchResults extends Fragment {
+
+    private FragmentHomeBinding binding;
+    private MovieAdapter movieAdapter;
+    private List<Movie> movieList;
+    private RecyclerView recyclerView;
+    String searchQuery;
+    ArrayList<String> selectedItems;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search_results, container, false);
+
+        Bundle bundle = getArguments();
+        if(bundle != null) {
+            searchQuery = bundle.getString("search_query");
+            selectedItems = bundle.getStringArrayList("filter_items");
+        }
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView search_results = view.findViewById(R.id.results_text);
+        if(searchQuery != null) {
+            search_results.setText("Search results for: " + searchQuery);
+        }
+        else {
+            search_results.setText("No search results");
+        }
+
+        //recyclerView = view.findViewById(R.id.RecyclerView);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        initData();
+
+        // Initialize data
+        //SetRecyclerView();
+    }
+
+    private void SetRecyclerView() {
+        //VersionsAdapter versionsAdapter = new VersionsAdapter(versionsList, requireContext());
+        recyclerView.setAdapter(movieAdapter);
+        recyclerView.setHasFixedSize(true);
+    }
+    private void initData() {
+
+        // Initialize movie list and add movies
+        movieList = new ArrayList<>();
+        movieList.add(new Movie("#1 Grown Ups", "Comedy", 5.0f, R.drawable.grown_ups));
+        movieList.add(new Movie("#2 Forrest Gump", "Drama", 5.0f, R.drawable.forrest_gump));
+        movieList.add(new Movie("#3 Interstellar", "Science Fiction", 4.5f, R.drawable.interstellar));
+        movieList.add(new Movie("#4 The Lion King", "Children's Animation", 4.5f, R.drawable.lion_king));
+        movieList.add(new Movie("#5 Ferris Bueller's Day Off", "Comedy", 4.5f, R.drawable.ferris));
+        movieList.add(new Movie("#6 Dumb and Dumber", "Comedy", 4.0f, R.drawable.dumb_dumber));
+        movieList.add(new Movie("#7 Step Brothers", "Comedy", 3.5f, R.drawable.step_brothers));
+        movieList.add(new Movie("#8 Crazy Rich Asians", "Romantic Comedy", 3.5f, R.drawable.crazy_rich_asians));
+        movieList.add(new Movie("#9 How to Lose a Guy in 10 Days", "Romantic Comedy", 3.5f, R.drawable.how_to_lose_a_guy));
+        movieList.add(new Movie("#10 10 Things I Hate About You", "Romantic Comedy", 3.0f, R.drawable.ten_things));
+
+        // Filter the movie list based on selectedItems
+        if (selectedItems != null && !selectedItems.isEmpty()) {
+            movieList.removeIf(movie -> !selectedItems.contains(movie.getGenre()));
+        }
+
+        // Initialize VersionsAdapter before using it
+        movieAdapter = new MovieAdapter(movieList);
+
+        //recyclerView.setAdapter(movieAdapter);
+
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
