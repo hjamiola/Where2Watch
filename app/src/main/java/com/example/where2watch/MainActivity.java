deb61694be8c714536ba5bb83c5bc111e9efaf68
Hayley Jamiola <51390401+hjamiola@users.noreply.github.com>
2024-11-06 14:57:04 -0600

    login screen and navigation drawer update to display username (#2)

--- app/src/main/java/com/example/where2watch/MainActivity.java
+++ app/src/main/java/com/example/where2watch/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.where2watch;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.where2watch.ui.adapter.MovieAdapter;
 import com.example.where2watch.ui.model.Movie;
@@ -55,7 +60,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Retrieve the saved username from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "Guest");
+
+        // Find the header view and set the username
+        View headerView = navigationView.getHeaderView(0);
+        TextView usernameTextView = headerView.findViewById(R.id.usernameTextView);
+        TextView favoriteGenreView = headerView.findViewById(R.id.fav_genre);
+        TextView loginOrOutButton = headerView.findViewById(R.id.logoutButton);
+        usernameTextView.setText(username);
+        if (username.equalsIgnoreCase("Guest")) {
+            favoriteGenreView.setText("Log in to view top genre.");
+            loginOrOutButton.setText("Login");
+        } else {
+            favoriteGenreView.setText("Top Genre: Comedy");
+            loginOrOutButton.setText("Logout");
+        }
+
+        // Set up the logout button
+        Button logoutButton = headerView.findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Clear the saved username in SharedPreferences
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.remove("username");
+                editor.apply();
+
+                // Go back to the LoginActivity
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Close MainActivity so the user can't go back
+            }
+        });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
