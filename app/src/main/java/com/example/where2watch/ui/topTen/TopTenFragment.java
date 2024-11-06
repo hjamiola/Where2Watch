ff48d6c3f3586eeace6d1b9dfd8ff18b70e0ae17
Allan Akkathara <allanakk248@gmail.com>
2024-11-06 13:56:28 -0600

    TopTen allan

--- app/src/main/java/com/example/where2watch/ui/topTen/TopTenFragment.java
+++ app/src/main/java/com/example/where2watch/ui/topTen/TopTenFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.where2watch.databinding.FragmentSlideshowBinding;
+import com.example.where2watch.databinding.FragmentToptenBinding;
 
 public class TopTenFragment extends Fragment {
 
-    private FragmentSlideshowBinding binding;
+    private FragmentToptenBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         TopTenViewModel topTenViewModel =
                 new ViewModelProvider(this).get(TopTenViewModel.class);
 
-        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        binding = FragmentToptenBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textSlideshow;
-        topTenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.texttopTen;
+//        topTenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
