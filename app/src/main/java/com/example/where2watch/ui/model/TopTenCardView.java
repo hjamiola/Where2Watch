cd247eaaba80f4dd64f57f114d726964ffd48aad
Allan Akkathara <allanakk248@gmail.com>
2024-11-10 22:52:06 -0600

    TopTen dynamic allan

---
+++ app/src/main/java/com/example/where2watch/ui/model/TopTenCardView.java
@@ -0,0 +1,108 @@
+package com.example.where2watch.ui.model;
+// CustomCardView.java
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.cardview.widget.CardView;
+import com.example.where2watch.R;
+
+public class TopTenCardView extends CardView {
+    private CardView movieCard;
+    private ImageView movieImage;
+    private TextView movieTitle;
+    private TextView movieNumber;
+
+    public TopTenCardView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public TopTenCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public TopTenCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        // Inflate the layout
+        LayoutInflater.from(context).inflate(R.layout.custom_card_view, this, true);
+
+        // Initialize views
+        movieCard = findViewById(R.id.movie_card_action_1);
+        movieImage = findViewById(R.id.movie_image);
+        movieTitle = findViewById(R.id.movie_title);
+        movieNumber = findViewById(R.id.movie_number);
+
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MovieCardView);
+            try {
+                // Set image
+                int imageResId = a.getResourceId(R.styleable.MovieCardView_movieImage, -1);
+                if (imageResId != -1) {
+                    movieImage.setImageResource(imageResId);
+                }
+
+                // Set title if provided
+                String title = a.getString(R.styleable.MovieCardView_movieTitle);
+                if (title != null) {
+                    movieTitle.setText(title);
+                }
+
+                // Set number if provided
+                String number = a.getString(R.styleable.MovieCardView_movieNumber);
+                if (number != null) {
+                    movieNumber.setText(number);
+                }
+
+                // Set card background tint if provided
+                int backgroundTint = a.getColor(R.styleable.MovieCardView_cardBackgroundTint,
+                        getResources().getColor(android.R.color.transparent));
+                movieCard.setCardBackgroundColor(backgroundTint);
+
+            } finally {
+                a.recycle();
+            }
+        }
+    }
+    public void setMovieData(Movie movie, int position) {
+        if (movie != null) {
+            setMovieImage(movie.getImageResId());
+            setMovieTitle(movie.getTitle());
+            setMovieNumber(String.valueOf(position + 1) + ".");
+        }
+    }
+
+    // Setter methods auto
+    public void setMovieImage(int resId) {
+        movieImage.setImageResource(resId);
+    }
+    public void setMovieTitle(String title) {
+        movieTitle.setText(title);
+    }
+    public void setMovieNumber(String number) {
+        movieNumber.setText(number);
+    }
+    public void setCardBackgroundTint(int color) {
+        movieCard.setCardBackgroundColor(color);
+    }
+    //getter methods auto
+    public ImageView getMovieImage() {
+        return movieImage;
+    }
+    public TextView getMovieTitle() {
+        return movieTitle;
+    }
+    public TextView getMovieNumber() {
+        return movieNumber;
+    }
+    public CardView getMovieCard() {
+        return movieCard;
+    }
+}
--- app/src/main/java/com/example/where2watch/ui/topTen/TopTenFragment.java
+++ app/src/main/java/com/example/where2watch/ui/topTen/TopTenFragment.java
@@ -1,33 +1,115 @@
 package com.example.where2watch.ui.topTen;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import com.example.where2watch.R;
 
 import com.example.where2watch.databinding.FragmentToptenBinding;
+import com.example.where2watch.ui.model.Movie;
+import com.example.where2watch.ui.model.TopTenCardView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TopTenFragment extends Fragment {
 
-    private FragmentToptenBinding binding;
+    private LinearLayout[] containers;
+    private List<List<Movie>> movieLists;  // Array of movie lists
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        TopTenViewModel topTenViewModel =
-                new ViewModelProvider(this).get(TopTenViewModel.class);
 
-        binding = FragmentToptenBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-//        final TextView textView = binding.texttopTen;
-//        topTenViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_topten, container, false);
+        containers = new LinearLayout[]{
+                view.findViewById(R.id.top_ten_container_action),
+                view.findViewById(R.id.top_ten_container_sci_fi),
+                view.findViewById(R.id.top_ten_container_horror)
+        };
+        initializeMovieList();
+        for (int i = 0; i < containers.length; i++) {
+            populateTopTen(movieLists.get(i), containers[i]);
+        }
+        return view;
     }
+    private void initializeMovieList() {
+        movieLists = new ArrayList<>();
+        List<Movie> actionMovies = new ArrayList<>();
+        List<Movie> sciFiMovies = new ArrayList<>();
+        List<Movie> horrorMovies = new ArrayList<>();
+
+        actionMovies.add(new Movie("The Fall Guy", "action", 5.0f, R.drawable.thefallguy));
+        actionMovies.add(new Movie("Rebel Ridge", "action", 5.0f, R.drawable.rebelridge));
+        actionMovies.add(new Movie("The Killer", "action", 4.5f, R.drawable.thekiller));
+        actionMovies.add(new Movie("Hit Man", "action", 4.5f, R.drawable.hitman));
+        actionMovies.add(new Movie("Twisters", "action", 4.5f, R.drawable.twisters));
+        actionMovies.add(new Movie("Baby Driver", "action", 4.5f, R.drawable.babydriverjpg));
+        actionMovies.add(new Movie("Borderlands", "action", 4.5f, R.drawable.borderlands));
+        actionMovies.add(new Movie("Kingdom of the Planet of the Apes", "action", 4.5f, R.drawable.planetoftheapes));
+        actionMovies.add(new Movie("Extraction 2", "action", 4.5f, R.drawable.extraction2));
+        actionMovies.add(new Movie("Madame Web", "action", 4.5f, R.drawable.mamdameweb));
+
+        sciFiMovies.add(new Movie("Interstellar", "sci-fi", 5.0f, R.drawable.interstellar));
+        sciFiMovies.add(new Movie("The Wild Robot", "sci-fi", 5.0f, R.drawable.thewildrobot));
+        sciFiMovies.add(new Movie("The Martian", "sci-fi", 5.0f, R.drawable.martian));
+        sciFiMovies.add(new Movie("Ad Astra", "sci-fi", 5.0f, R.drawable.adastra));
+        sciFiMovies.add(new Movie("Arrival", "sci-fi", 5.0f, R.drawable.arrival));
+        sciFiMovies.add(new Movie("Gravity", "sci-fi", 5.0f, R.drawable.gravity));
+        sciFiMovies.add(new Movie("Alien: Romulus", "sci-fi", 5.0f, R.drawable.alienromulus));
+        sciFiMovies.add(new Movie("Blade Runner 2049", "sci-fi", 5.0f, R.drawable.bladerunner2049));
+        sciFiMovies.add(new Movie("Deadpool & Wolverine", "sci-fi", 5.0f, R.drawable.deadpoolwolverine));
+        sciFiMovies.add(new Movie("Afraid", "sci-fi", 5.0f, R.drawable.afraid));
+
+        horrorMovies.add(new Movie("Don't Move", "horror", 5.0f, R.drawable.dontmove1));
+        horrorMovies.add(new Movie("Smile 2", "horror", 5.0f, R.drawable.smile2));
+        horrorMovies.add(new Movie("Apartment 7A", "horror", 5.0f, R.drawable.apt7a));
+        horrorMovies.add(new Movie("Longlegs", "horror", 5.0f, R.drawable.longlegs));
+        horrorMovies.add(new Movie("Oddity", "horror", 5.0f, R.drawable.oddity));
+        horrorMovies.add(new Movie("Cuckoo", "horror", 5.0f, R.drawable.cuckoo));
+        horrorMovies.add(new Movie("Trap", "horror", 5.0f, R.drawable.trap));
+        horrorMovies.add(new Movie("The Watchers", "horror", 5.0f, R.drawable.thewatchers));
+        horrorMovies.add(new Movie("The Substsance", "horror", 5.0f, R.drawable.thesubstance));
+        horrorMovies.add(new Movie("The Deliverance", "horror", 5.0f, R.drawable.thedeliverance));
+
+        movieLists.add(actionMovies);
+        movieLists.add(sciFiMovies);
+        movieLists.add(horrorMovies);
+    }
+
+    private void populateTopTen(List<Movie> movies, LinearLayout container) {
+        if (movies == null || movies.isEmpty() || container == null) {
+            return;
+        }
+        container.removeAllViews(); // Clear existing views
+        for (int i = 0; i < movies.size(); i++) {
+            Movie movie = movies.get(i);
+            TopTenCardView cardView = new TopTenCardView(requireContext());
+
+            cardView.setMovieImage(movie.getImageResId());
+            cardView.setMovieTitle(movie.getTitle());
+            cardView.setMovieNumber(String.valueOf(i + 1) + ".");
+
+            container.addView(cardView);
+        }
+    }
+
+//    public void updateMovieLists(List<List<Movie>> newMovieLists) { //use this to add a new movielist for the actual app.
+//        movieLists = newMovieLists;
+//        for (int i = 0; i < containers.length; i++) {
+//            populateTopTen(movieLists.get(i), containers[i]);
+//        }
+//    }
+
+    private FragmentToptenBinding binding;
 
     @Override
     public void onDestroyView() {
